4495b80f89605a1f52e3516b227c30f80d54d43b
gmitis <gi.muths@gmail.com>
2024-10-06 17:27:01 +0300

    Added Chemical, SafetyFeautures, Materials models and fields to Battery model

--- battery2life/batteries/models.py
+++ battery2life/batteries/models.py
@@ -1,3 +1,8 @@
+
+# todo: add SafetyFeatures extinguising_agent choices  
+# todo(optional): add envirnmental, manufacturing fields/models
+# todo(optional): add Battery.recycling_info Recycling(critical_raw_materials, safety_measures, dismantling_info(processes), info_prevention_management)
+
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
@@ -11,7 +16,7 @@ class Address(models.Model):
     class Meta:
         verbose_name_plural = "Addresses"
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"{self.city} {self.street} {self.number}"
 
 class Dimensions(models.Model):
@@ -20,12 +25,47 @@ class Dimensions(models.Model):
     width = models.DecimalField(max_digits=4, decimal_places=2, null=True)
     length = models.DecimalField(max_digits=4, decimal_places=2, null=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return f"height:{self.height} width:{self.width} length:{self.length}"
     
 class Chemical(models.Model):
+    EXTINGUISING_AGENTS =[
+        ('fe', 'fire extinguiser'),
+    ]
+    
     hazardus = models.BooleanField(default=False)
+    extinguising_agent = models.CharField(
+        max_length=3, 
+        choices=EXTINGUISING_AGENTS, 
+        blank=True, 
+        null=True
+    )
     chemical_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.chemical_name
+
+    
+class SafetyFeatures(models.Model):
+    SAFETY_FEATURES = [
+        ('tm', 'thermal management'),
+        ('op', 'overcharge protection'),
+        ('sc', 'short-circuit prevention'),
+    ]
+    
+    safety_feature = models.CharField(max_length=2, choices=SAFETY_FEATURES, null=True)
+    
+    def __str__(self):
+        return self.safety_feature
+
+class Materials(models.Model):
+    critical_material = models.BooleanField(default=False)
+    recycled_material = models.BooleanField(default=False)
+    material = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.material
+
 
 
 class Manufacturer(models.Model):
@@ -43,9 +83,38 @@ class Battery(models.Model):
     weight = models.DecimalField(decimal_places=3, max_digits=10, blank=True, null=True, default=0.1) # kilograms
     capacity = models.IntegerField(blank=True, null=True, default=0)  # amp-hours
     original_power_capability = models.IntegerField(blank=True, null=True, default=0)  # Watts
-    manufacture_date = models.DateField()
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="batteries", unique=True)
-    battery_dimensions = models.ForeignKey(Dimensions, null=True, on_delete=models.PROTECT, unique=True)
+    expected_EndOfLife= models.DateField(blank=True, null=True)
+    manufactured_date = models.DateField()
+    manufactured_place = models.ForeignKey(
+        Address, 
+        on_delete=models.PROTECT, 
+        blank=True, 
+        null=True
+    )
+    materials = models.ForeignKey(
+        Materials, 
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name='batteries'
+    )
+    safety_features = models.ForeignKey(
+        SafetyFeatures, 
+        on_delete=models.PROTECT,
+        blank=True, 
+        null=True, 
+        related_name='batteries'
+    )
+    manufacturer = models.OneToOneField(
+        Manufacturer, 
+        on_delete=models.CASCADE, 
+        related_name="batteries", 
+    )
+    battery_dimensions = models.OneToOneField(
+        Dimensions, 
+        on_delete=models.PROTECT, 
+        null=True, 
+    )
 
     class Meta:
         verbose_name_plural = "Batteries"
@@ -70,9 +139,8 @@ class Module(models.Model):
     nominal_voltage = models.IntegerField(blank=True, null=True, default=0)
     # Unit V
     operating_voltage_range = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(1000)],)
-    communication = models.CharField(blank=True, null=True, max_length=255
-    )
-        # Unit kg
+    communication = models.CharField(blank=True, null=True, max_length=255)
+    # Unit kg
     power_module_weight = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=1.0)
     # Unit kg
     battery_module_weight = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True, default=1.0 )
@@ -89,8 +157,18 @@ class Module(models.Model):
     cell_technology = models.CharField(blank=True, null=True, max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    battery_module_dimension = models.ForeignKey(Dimensions, null=True, on_delete=models.PROTECT, unique=True, related_name='battery_module_dimension')
-    power_module_dimension = models.ForeignKey(Dimensions, null=True, on_delete=models.PROTECT, unique=True, related_name='power_module_dimension')
+    battery_module_dimension = models.OneToOneField(
+        Dimensions,
+        null=True, 
+        on_delete=models.PROTECT, 
+        related_name='battery_module_dimension'
+    )
+    power_module_dimension = models.OneToOneField(
+        Dimensions,
+        null=True, 
+        on_delete=models.PROTECT, 
+        related_name='power_module_dimension'
+    )
     battery = models.ForeignKey(Battery, on_delete=models.CASCADE, related_name="modules")
 
     class Meta:
@@ -147,8 +225,8 @@ class Cell(models.Model):
         decimal_places=2,
         validators=[MinValueValidator(-30.0), MaxValueValidator(60.0)],
     )
-    cell_dimension = models.ForeignKey(Dimensions, null=True, unique=True, on_delete=models.PROTECT)
-    cell_chemistry = models.ManyToManyField(Chemical, null=True, blank=True, related_name='cells')
+    cell_dimension = models.OneToOneField(Dimensions, null=True, on_delete=models.PROTECT)
+    cell_chemistry = models.ManyToManyField(Chemical, blank=True, related_name='cells')
     module = models.ForeignKey(Module, on_delete=models.CASCADE, related_name="cells")
 
     class Meta:
