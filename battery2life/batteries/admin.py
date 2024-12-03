2e79ebae4a2130638ac69ce1e25a1e56f9a8fc8c
gmitis <gi.muths@gmail.com>
2024-12-03 14:12:35 +0200

    Added all of the models to the admin interface

--- battery2life/batteries/admin.py
+++ battery2life/batteries/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Manufacturer, Battery
+from .models import EIS, Cell, Manufacturer, Battery, Measurement, Module
 
 admin.site.register(Manufacturer)
 admin.site.register(Battery)
+admin.site.register(Module)
+admin.site.register(Cell)
+admin.site.register(Measurement)
+admin.site.register(EIS)
